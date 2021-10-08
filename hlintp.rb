72cbfa5e333b711530a409e2aeb5cf0f33ee728e
Victor Maslov <nakilon@gmail.com>
2021-10-08 13:08:56 +0300

    indentation

--- hlintp.rb
+++ hlintp.rb
@@ -7,37 +7,37 @@ module HanoiLove
   # ResultStruct = Struct.new :stdout, :stack, :exitcode
   def self.run code, stdout = StringIO.new, stdin = STDIN
 
-# # Record when this starts, for specs after run
-# msec = Time.now.to_f * 1000;
+    # # Record when this starts, for specs after run
+    # msec = Time.now.to_f * 1000;
 
-cp = -1 # code pointer
+    cp = -1 # code pointer
 
-code.delete "^.',;`:!\""  # Hanoi Love commands are as follows: . ' , ; ` "  : !
+    code.delete "^.',;`:!\""  # Hanoi Love commands are as follows: . ' , ; ` "  : !
 
-# Make cl exist now
-cl = code.length # cleaned code length
+    # Make cl exist now
+    cl = code.length # cleaned code length
 
-# HL specific stuff
-rg = 0 # value in register (0-255)
-sa, sb, sc, sd = [], [], []. []
-st = [sa, sb, sc, sd] # current stack (for A-D)
+    # HL specific stuff
+    rg = 0 # value in register (0-255)
+    sa, sb, sc, sd = [], [], []. []
+    st = [sa, sb, sc, sd] # current stack (for A-D)
 
-# Interpreting!
-until cl == cp += 1
-  case cleaned[cp]
-    when ?.  then st = (st + 1) % 4
-    when ?'  then # print "\'"
-    when ?,  then # print ","
-    when ?;  then # print ";"
-    when ?`  then # print "`"
-    when ?"  then # print "\""
-    when ?:  then # print ":"
-    when ?!  then # print "!"
-  end
-end
+    # Interpreting!
+    until cl == cp += 1
+      case cleaned[cp]
+        when ?.  then st = (st + 1) % 4
+        when ?'  then # print "\'"
+        when ?,  then # print ","
+        when ?;  then # print ";"
+        when ?`  then # print "`"
+        when ?"  then # print "\""
+        when ?:  then # print ":"
+        when ?!  then # print "!"
+      end
+    end
 
-# Benchmarking
-runtime = Time.now.to_f * 1000 - msec
+    # Benchmarking
+    runtime = Time.now.to_f * 1000 - msec
 
   end
 end
